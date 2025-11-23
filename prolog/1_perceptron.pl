6e0ce54a7772d73ae1520b50cd4dd35a233def1d
dyluc <dylanlucas1510@gmail.com>
2025-11-23 17:22:28 -0500

    Rename prolog script 1

--- prolog/1_perceptron.pl
+++ prolog/1_perceptron.pl
@@ -85,6 +85,10 @@ all_risk_factors([
     pneumonia, immunosuppressed, hypertension, chronic_kidney_disease,
     asthma, obesity, diabetes, cardiovascular_disease]).
 
+% Sex risk weights (males slightly more at risk)
+sex_risk_weight(male, 0.023).
+sex_risk_weight(female, 0.015).
+
 % Define patients symptoms as a list of atoms
 patient_symptoms(patient1, [fever, dry_cough, sore_throat]).
 patient_symptoms(patient2, [hyposmia, runny_nose]).
@@ -105,6 +109,12 @@ patient_risk_factor(patient5, diabetes).
 patient_risk_factor(patient5, cardiovascular_disease).
 patient_risk_factor(patient8, asthma).
 
+% Define patient ages and sex
+patient_age(patient1, 72).
+patient_age(patient2, 45).
+patient_sex(patient1, male).
+patient_sex(patient2, female).
+
 % == Rules ==
 % 1. Rules used to determine if patient symptom list contains low, medium, or high severity symptoms.
 % Using predicate https://www.swi-prolog.org/pldoc/man?predicate=member/2
@@ -122,7 +132,8 @@ contains_symptom_with_severity([_|Ss], Severity) :-
 
 % ----
 
-% 2. Rules to determine if a given patient has any symptoms of a specific severity, or any given risk factor.
+% 2. Rules to determine if a given patient has any symptoms of a specific severity, or any given risk factor. Also
+% included are rules to assign weights for a given age (the only continous value).
 
 % 2.1 Symptoms of severity
 % NOT USED, JUST TO SHOWCASE
@@ -133,6 +144,16 @@ patient_has_symptom_with_severity(Patient, Severity) :-
 % 2.2 Risk factors
 patient_has_risk_factor(Patient, RiskFactor) :-
     patient_risk_factor(Patient, RiskFactor). % If fact defined
+
+% 2.3 Age risk (rough boundaries derived from decision tree analysis)
+age_risk_weight(Age, Weight) :-
+    Age < 5, Weight = 0.10. % Higher risk for infants
+age_risk_weight(Age, Weight) :-
+    Age >= 5, Age < 68, Weight = 0.03. % Baseline minimal risk
+age_risk_weight(Age, Weight) :-
+    Age >= 68, Age < 75, Weight = 0.15. % High risk for elderly (significant split at 67.5)
+age_risk_weight(Age, Weight) :-
+    Age >= 75, Weight = 0.234. % Significant risk for very elderly
 
 % ----
 
@@ -145,7 +166,7 @@ count_symptoms_by_severity(Patient, Severity, Count) :-
 % ----
 
 % 4. Rules to produce a weighted sum of patient severities and risk factors, allowing relative scoring without hardcoding
-% all possible combinations, using the accumulator pattern.
+% all possible combinations, using the accumulator pattern. Also included are rules to score patient age and sex.
 
 % 4.1 In the base case, if the severity list is empty, unify accumulator with final score
 score_patient_severities_acc(_, [], Acc, Acc).
@@ -184,13 +205,31 @@ score_patient_risk_factors(Patient, Score) :-
     all_risk_factors(AllRiskFactors),
     score_patient_risk_factors_acc(Patient, AllRiskFactors, 0, Score).
 
+% 4.7 Produce a score for the patient age
+score_patient_age(Patient, Score) :-
+    patient_age(Patient, Age),
+    age_risk_weight(Age, Score),
+    !.
+score_patient_age(_, 0). % Without data available, there should be 0 contribution
+
+% 4.8 Produce a score for the patient sex
+score_patient_sex(Patient, Score) :-
+    patient_sex(Patient, Sex),
+    sex_risk_weight(Sex, Score),
+    !.
+score_patient_sex(_, 0). % Without data available, there should be 0 contribution
+
 % ----
 
-% 5. Rule to aggregate scores for patient severities and risk factors
+% 5. Rule to aggregate scores for patient severities, risk factors, and bio data (sex and age)
 score_patient(Patient, TotalScore) :-
     score_patient_severities(Patient, SymptomScore),
     score_patient_risk_factors(Patient, RiskScore),
-    TotalScore is (0.6 * SymptomScore) + (0.4 * RiskScore). % 60/40 symptom/risk relative contribution
+    score_patient_age(Patient, AgeScore),
+    score_patient_sex(Patient, SexScore),
+    % 60%/32%/6%/2% symptom/risk/age/sex relative contribution
+    TotalScore is (0.6 * SymptomScore) + (0.32 * RiskScore) + (0.06 * AgeScore) + (0.02 * SexScore),
+    !.
 
 % ----
 
@@ -229,7 +268,7 @@ patient_infected(_, 0.0, 0).
 
 /* 
 
-score_patient(patient1, Score1), patient_infected(patient1, Proba1, Class1),
+?- score_patient(patient1, Score1), patient_infected(patient1, Proba1, Class1),
 score_patient(patient2, Score2), patient_infected(patient2, Proba2, Class2),
 score_patient(patient3, Score3), patient_infected(patient3, Proba3, Class3),
 score_patient(patient4, Score4), patient_infected(patient4, Proba4, Class4),
@@ -238,24 +277,23 @@ score_patient(patient6, Score6), patient_infected(patient6, Proba6, Class6),
 score_patient(patient7, Score7), patient_infected(patient7, Proba7, Class7),
 score_patient(patient8, Score8), patient_infected(patient8, Proba8, Class8).
 
-Score1 = 0.39360000000000006,
-Proba1 = 0.5482493912654847,
+Score1 = 0.34234000000000003,
+Proba1 = 0.5355250401347063,
 Class1 = Class3, Class3 = Class4, Class4 = Class6, Class6 = Class7, Class7 = 1,
-Score2 = 0.112,
-Proba2 = 0.47801418634752707,
+Score2 = 0.1109,
+Proba2 = 0.4777397247264924,
 Class2 = Class5, Class5 = Class8, Class8 = 0,
 Score3 = 0.528,
 Proba3 = 0.5812726666092255,
-Score4 = 1.2511999999999999,
-Proba4 = 0.7410052655322968,
-Score5 = 0.3148,
+Score4 = 1.19296,
+Proba4 = 0.7296721811592222,
+Score5 = 0.25184,
 Proba5 = 0.0,
 Score6 = 0.207,
 Proba6 = 0.5017499928542016,
 Score7 = 0.48,
 Proba7 = 0.569546223939229,
-Score8 = 0.0624,
-Proba8 = 0.4656541742088314 ;
-false.
+Score8 = 0.05952,
+Proba8 = 0.46493764293144785.
 
 */
